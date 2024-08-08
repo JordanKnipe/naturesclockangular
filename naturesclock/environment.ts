bf2de2805be39527fa26d7c6cb2fac80984c38b0
JordanKnipe <69141927+JordanKnipe@users.noreply.github.com>
2024-08-08 11:26:45 +1000

    added dev cognito client

--- naturesclock/environment.ts
+++ naturesclock/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    cognitoDomain:'https://naturesclock.auth.ap-southeast-2.amazoncognito.com',
-    clientId: '5l9cft3jl63f9g8f1r1a1te9j1',
+    cognitoDomain: 'https://dev-naturesclock.auth.ap-southeast-2.amazoncognito.com', // dev domain
+    clientId: '29se6ivliuc914iqnh1170hhpc',  //dev client
     redirectUri: 'http://localhost:4200/login-callback'
   };
